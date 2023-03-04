c3ca0de8a771a54f878fab65bc96508c492230b7
Luis Choque <luis.choque.castro@outlook.com>
2023-03-04 17:35:49 -0500

    test2

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 // import "https://deno.land/std@0.163.0/dotenv/load.ts";
-import { startServer } from "./backend/server.ts";
+// import { startServer } from "./backend/server.ts";
 
-await startServer();
+// await startServer();
