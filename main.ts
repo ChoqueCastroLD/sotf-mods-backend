566b455316ee4d50a1b0e8dc7306a83ae05bff0e
Luis Choque <luis.choque.castro@outlook.com>
2023-03-04 17:41:33 -0500

    Test4

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import "https://deno.land/std@0.163.0/dotenv/load.ts";
-import { startServer } from "./backend/server.ts";
+// import "https://deno.land/std@0.163.0/dotenv/load.ts";
+// import { startServer } from "./backend/server.ts";
 
-await startServer();
+// await startServer();
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
