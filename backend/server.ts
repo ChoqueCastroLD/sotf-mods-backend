3dab09e681bab8eb700511797a895ff2132022de
Luis Choque <luis.choque.castro@outlook.com>
2023-03-04 18:51:05 -0500

    Fixes

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,18 @@
 import "./util/env.ts";
-import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { Application, Router, send  } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { renderFile } from "https://deno.land/x/pug/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 import { Options, LocalsObject } from "https://cdn.esm.sh/v56/pug@3.0.2/deno/pug.bundle.js";
 import { getMod, getMods } from "./services/mods.ts";
 
-/**
- * Start server.
- */
+
+function renderPug(template_name: string, data: Options & LocalsObject) {
+  const filepath = path.join(Deno.cwd(), "frontend", `${template_name}.pug`);
+  console.log(filepath);
+  const result = renderFile(filepath, data);
+  console.log(result);
+  return result;
+}
 
 export async function startServer() {
   /**
@@ -17,11 +22,19 @@ export async function startServer() {
   const app = new Application();
   const router = new Router();
 
-  function renderPug(template_name: string, data: Options & LocalsObject) {
-    const filepath = path.join(Deno.cwd(), "frontend", `${template_name}.pug`);
-    return renderFile(filepath, data);
-  }
-
+  app.use(async (context, next) => {
+    console.log({pathname: context.request.url.pathname});
+    if (!context.request.url.pathname.startsWith("/static")) {
+      console.log('calling next');
+      
+      next();
+      return;
+    }
+    const filePath = context.request.url.pathname.replace("/static", "");
+    await send(context, filePath, {
+      root: path.join(Deno.cwd(), "/frontend/static"),
+    });
+  });
 
   /**
    * Setup routes.
@@ -34,7 +47,10 @@ export async function startServer() {
       context.response.body = renderPug("upload", {});
     })
     .get("/", async (context) => {
+      console.log('next')
       const mods = await getMods();
+      console.log({mods});
+      
       context.response.body = renderPug("index", {
         mods,
       });
