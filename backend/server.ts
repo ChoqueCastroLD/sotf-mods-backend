bb4a1d71ef251ef481284b88cb912c9acbe1a146
Luis Choque <luis.choque.castro@outlook.com>
2023-03-04 17:39:26 -0500

    test3

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,9 @@
-import { prisma } from "./services/prisma.ts";
+// import { prisma } from "./services/prisma.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { renderFile } from "https://deno.land/x/pug/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
-import { Options,LocalsObject } from "https://cdn.esm.sh/v56/pug@3.0.2/deno/pug.bundle.js";
-import { decorateMod, getMods } from "./services/mods.ts";
+import { Options, LocalsObject } from "https://cdn.esm.sh/v56/pug@3.0.2/deno/pug.bundle.js";
+// import { decorateMod, getMods } from "./services/mods.ts";
 
 /**
  * Start server.
@@ -30,38 +30,38 @@ export async function startServer() {
     .get("/how-to-mod", async (context) => {
       context.response.body = renderPug("how-to-mod");
     })
-    .get("/upload", async (context) => {
-      context.response.body = renderPug("upload");
-    })
-    .get("/", async (context) => {
-      const mods = await getMods();
-      context.response.body = renderPug("index", {
-        mods,
-      });
-    })
-    .get("/mod/:user_slug/:mod_slug", async (context) => {
-      const mod = await prisma.mod.findFirst({
-        where: {
-          slug: context.params.mod_slug,
-          user: {
-            slug: context.params.user_slug,
-          }
-        },
-        include: {
-          images: true,
-          user: true,
-          versions: true,
-          _count: {
-            select: {
-              favorites: true,
-            }
-          }
-        },
-      });
-      context.response.body = renderPug("mod", {
-        mod: await decorateMod(mod),
-      });
-    })
+    // .get("/upload", async (context) => {
+    //   context.response.body = renderPug("upload");
+    // })
+    // .get("/", async (context) => {
+    //   const mods = await getMods();
+    //   context.response.body = renderPug("index", {
+    //     mods,
+    //   });
+    // })
+    // .get("/mod/:user_slug/:mod_slug", async (context) => {
+    //   const mod = await prisma.mod.findFirst({
+    //     where: {
+    //       slug: context.params.mod_slug,
+    //       user: {
+    //         slug: context.params.user_slug,
+    //       }
+    //     },
+    //     include: {
+    //       images: true,
+    //       user: true,
+    //       versions: true,
+    //       _count: {
+    //         select: {
+    //           favorites: true,
+    //         }
+    //       }
+    //     },
+    //   });
+    //   context.response.body = renderPug("mod", {
+    //     mod: await decorateMod(mod),
+    //   });
+    // })
   /**
    * Setup middleware.
    */
