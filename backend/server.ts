2ebbe7db3d4d79acb31675ad659758d0dc05269d
Luis Choque <luis.choque.castro@outlook.com>
2023-03-05 00:49:19 -0500

    Fixed readfilesync call

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,14 @@
 // deno-lint-ignore-file
 import "./util/env.ts";
 import { Application, Router  } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { renderFile } from "https://cdn.jsdelivr.net/gh/lumeland/pug@master/mod.ts";
+import { renderFileAsync } from "https://deno.land/x/pug_async@first_release/mod.ts";
 import * as path from "https://deno.land/std@0.178.0/path/mod.ts";
 import { getMod, getMods } from "./services/mods.ts";
 import { staticAssetsMiddleware } from "./middlewares/static.ts";
 
-function renderPug(template_name: string, data: any) {
+async function renderPug(template_name: string, data: any) {
   const filepath = path.join(Deno.cwd(), "frontend", `${template_name}.pug`);
-  return renderFile(filepath, data) as string;
+  return await renderFileAsync(filepath, data) as string;
 }
 
 export async function startServer() {
@@ -18,21 +18,21 @@ export async function startServer() {
   app.use(staticAssetsMiddleware);
 
   router
-    .get("/how-to-mod", (context) => {
-      context.response.body = renderPug("how-to-mod", {});
+    .get("/how-to-mod", async (context) => {
+      context.response.body = await renderPug("how-to-mod", {});
     })
-    .get("/upload", (context) => {
-      context.response.body = renderPug("upload", {});
+    .get("/upload", async (context) => {
+      context.response.body = await renderPug("upload", {});
     })
     .get("/", async (context) => {
       const mods = await getMods();
-      context.response.body = renderPug("index", {
+      context.response.body = await renderPug("index", {
         mods,
       });
     })
     .get("/mod/:user_slug/:mod_slug", async (context) => {
       const mod = await getMod(context.params.mod_slug, context.params.user_slug);
-      context.response.body = renderPug("mod", {
+      context.response.body = await renderPug("mod", {
         mod,
       });
     })
